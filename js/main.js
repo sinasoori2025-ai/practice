31330aeaa210babf64413301e3c52fb5a65fbee2
sinasoori2025-ai <sinasoori2025@gmail.com>
2026-07-06 01:22:03 +0330

    practice

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const form=document.getElementById("form");
+const nameInput=document.getElementById("name");
+const phoneInput=document.getElementById("phone");
+
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+
+    if(nameInput.value.trim()===""||phoneInput.value.trim()===""){
+        alert("لطفاً اطلاعات را کامل وارد کنید.");
+    }else{
+        alert("ورود با موفقیت انجام شد.");
+    }
+});
